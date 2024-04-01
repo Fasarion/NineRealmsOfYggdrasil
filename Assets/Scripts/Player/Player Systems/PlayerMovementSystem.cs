bf6c481c0ba205279955b614273214c6011a4fd2
pythonpalle <patrikwlindfors@gmail.com>
2024-04-01 15:17:43 +0200

    ändringar i player prefab

--- Assets/Scripts/Player/Player Systems/PlayerMovementSystem.cs
+++ Assets/Scripts/Player/Player Systems/PlayerMovementSystem.cs
@@ -21,19 +21,13 @@ namespace Player
     
         public void OnUpdate(ref SystemState state)
         {
-            bool applySprint = Input.GetKey(KeyCode.LeftShift);
             var playerPosSingleton = SystemAPI.GetSingletonRW<PlayerPositionSingleton>();
             var moveInput = SystemAPI.GetSingleton<PlayerMoveInput>();
 
-            foreach (var (playerTransform, speedComp, sprintMod) 
-                in SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeedComponent>, RefRO<SprintComponent>>().WithAll<PlayerTag>())
+            foreach (var (playerTransform, speedComp) 
+                in SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeedComponent>>().WithAll<PlayerTag>())
             {
                 float speed = speedComp.ValueRO.Value;
-                if (applySprint)
-                {
-                    speed *= sprintMod.ValueRO.SprintModifier;
-                }
-        
                 var newPos = playerTransform.ValueRO.Position.xz +  moveInput.Value * speed * SystemAPI.Time.DeltaTime;
                 playerTransform.ValueRW.Position.xz = newPos;
                 playerPosSingleton.ValueRW.Value = playerTransform.ValueRO.Position;

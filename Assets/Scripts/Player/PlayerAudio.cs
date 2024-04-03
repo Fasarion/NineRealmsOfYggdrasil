0c62058d5c670f362f2b86f76dcb5c602e45788a
Alex - Swesie <e.alex.persson@gmail.com>
2024-04-03 11:11:21 +0200

    Audio Scriptable Objects

--- Assets/Scripts/Player/PlayerAudio.cs
+++ Assets/Scripts/Player/PlayerAudio.cs
@@ -5,28 +5,13 @@ using UnityEngine;
 //using FMOD.Studio;
 
 
-[CreateAssetMenu(menuName = "SO/Audio/Player", fileName = "New Player Sheet")]
+[CreateAssetMenu(menuName = "ScriptableObjects/Audio/Player")]
 public class PlayerAudio : ScriptableObject
 {
    
 
- /*   
-    [Header("Movement/VO")] 
-    public EventReference footsteps;
-
-    public EventReference takingDamage;
-
-    public EventReference playerDeath;
-    //[FMODUnity.EventRef] public string footsteps = null;
-
-    public EventReference dash;
-
-    [Header("Enemy")] 
-    public EventReference wetDeath;
-
-
-    [Header("Boss")] 
-    public EventReference death;
+ /*
+  Fotsteg, skada, death, dash
 
 */
 }

d203b90cd22b8a4ea8962409d6a2b76cf0d253b8
Fasarion <oscara_93@hotmail.com>
2024-04-03 11:35:05 +0200

    Some old enemy model and animation stuffs
    Check prefab folder for added revenant enemy without scripts

--- Assets/Individual/Oscar - Programmering/Scripts/GridManager.cs
+++ Assets/Individual/Oscar - Programmering/Scripts/GridManager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
+    
     [SerializeField] private int width;
     [SerializeField] private int height;
 
     [SerializeField] private Tile tilePrefab;
+    [SerializeField] Mesh targetMesh;
     // Start is called before the first frame update
 
     private void Start()
@@ -16,6 +18,14 @@ public class GridManager : MonoBehaviour
         GenerateGrid();
     }
 
+    private void GetMeshBounds()
+    {
+        var xExtents = targetMesh.bounds.extents.x;
+        var zExtents = targetMesh.bounds.extents.z;
+        
+        Debug.Log("xExtents: "+ xExtents);
+        Debug.Log("zExtents: "+ zExtents);
+    }
     void GenerateGrid()
     {
         for (int x = 0; x < width; x++)

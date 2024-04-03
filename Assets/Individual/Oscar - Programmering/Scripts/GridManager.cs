6962a3c53db189a72eeb9e01e0c8af7aec1a9658
Fasarion <oscara_93@hotmail.com>
2024-04-03 13:53:09 +0200

    Some grid work

--- Assets/Individual/Oscar - Programmering/Scripts/GridManager.cs
+++ Assets/Individual/Oscar - Programmering/Scripts/GridManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
@@ -10,29 +11,38 @@ public class GridManager : MonoBehaviour
     [SerializeField] private int height;
 
     [SerializeField] private Tile tilePrefab;
-    [SerializeField] Mesh targetMesh;
+    [SerializeField] GameObject targetObject;
     // Start is called before the first frame update
 
     private void Start()
     {
-        GenerateGrid();
+        var targetBounds = GetMeshBounds(targetObject);
+        var tileBounds = GetMeshBounds(tilePrefab.gameObject);
+        GenerateGrid(targetBounds.x, targetBounds.z, tileBounds);
+        
     }
 
-    private void GetMeshBounds()
+    private Vector3 GetMeshBounds(GameObject meshObject)
     {
-        var xExtents = targetMesh.bounds.extents.x;
-        var zExtents = targetMesh.bounds.extents.z;
-        
-        Debug.Log("xExtents: "+ xExtents);
-        Debug.Log("zExtents: "+ zExtents);
+        var targetMesh = meshObject.GetComponent<MeshFilter>().sharedMesh;
+        var targetScale = meshObject.transform.lossyScale;
+        Vector3 targetObjectExtents = new  Vector3(targetMesh.bounds.extents.x * targetScale.x, targetMesh.bounds.extents.y * targetScale.y, targetMesh.bounds.extents.z * targetScale.z) * 2;
+
+        return targetObjectExtents;
     }
-    void GenerateGrid()
+    void GenerateGrid(float gridWidth, float gridHeight, Vector3 tileSize)
     {
-        for (int x = 0; x < width; x++)
+        
+        
+        
+        int gridIntWidth = Mathf.CeilToInt(gridWidth / tileSize.x);
+        int gridIntHeight = Mathf.CeilToInt(gridHeight / tileSize.z);
+
+        for (int x = 0; x < gridIntWidth; x++)
         {
-            for (int y = 0; y < height; y++)
+            for (int y = 0; y < gridIntHeight; y++)
             {
-                var spawnedTile = Instantiate(tilePrefab, new Vector3(x, 0, y), Quaternion.identity);
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(x * tileSize.x, 0, y * tileSize.z), Quaternion.identity);
                 string tileName = "Tile " + x +  "," + y;
             }
         }

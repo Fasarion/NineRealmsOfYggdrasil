2a62576104c37ec32c2699dca58363d9f8e38b91
Alex - Swesie <e.alex.persson@gmail.com>
2024-04-10 20:44:38 +0200

    Music and Audio Things, new banks

--- Assets/Scripts/Audio/WeaponAudio.cs
+++ Assets/Scripts/Audio/WeaponAudio.cs
@@ -13,18 +13,49 @@ public class WeaponAudio : ScriptableObject
     public EventReference swordAudio;
 
     public EventReference hammerAudio;
-    
-    public EventInstance SwordInstance;
-    public EventInstance HammerInstance;
 
+    public EventReference meadAudio;
+    public EventReference crowAudio;
+
+    private EventInstance _swordInstance;
+    private EventInstance _hammerInstance;
+    private EventInstance _meadInstance;
+    private EventInstance _crowInstance;
     public void WeaponSwingAudio(int weapon, int attackType)
     {
         switch (weapon)
         {
-            case 0:
+            
+            case 1:
             {
-                SwordInstance = RuntimeManager.CreateInstance(swordAudio);
-                SwordInstance.setParameterByName("AttackType", attackType);
+                _swordInstance = RuntimeManager.CreateInstance(swordAudio);
+                _swordInstance.setParameterByName("AttackType", attackType);
+                _swordInstance.start();
+                _swordInstance.release();
+                break;
+            }
+            case 2:
+            {
+                _hammerInstance = RuntimeManager.CreateInstance(hammerAudio);
+                _hammerInstance.setParameterByName("AttackType", attackType);
+                _hammerInstance.start();
+                _hammerInstance.release();
+                break;
+            }
+            case 3:
+            {
+                _meadInstance = RuntimeManager.CreateInstance(meadAudio);
+                _meadInstance.setParameterByName("AttackType", attackType);
+                _meadInstance.start();
+                _meadInstance.release();
+                break;
+            }
+            case 4:
+            {
+                _crowInstance = RuntimeManager.CreateInstance(crowAudio);
+                _crowInstance.setParameterByName("AttackType", attackType);
+                _crowInstance.start();
+                _crowInstance.release();
                 break;
             }
         }

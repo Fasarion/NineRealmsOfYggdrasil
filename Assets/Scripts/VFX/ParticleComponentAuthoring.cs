c87627d07a5ec97ebfb2beb4b3e198146f713f60
Reciteless <123382507+Reciteless@users.noreply.github.com>
2024-04-13 03:16:26 +0200

    pushy

--- Assets/Scripts/VFX/ParticleComponentAuthoring.cs
+++ Assets/Scripts/VFX/ParticleComponentAuthoring.cs
@@ -1,23 +1,28 @@
-
+using System.Collections;
+using System.Collections.Generic;
 using Unity.Entities;
 using Unity.Transforms;
 using UnityEngine;
+using UnityEngine.VFX;
 
-
-public class AnimationAuthoring : MonoBehaviour
+public class ParticleComponentAuthoring : MonoBehaviour
 {
-    [Tooltip("A Game Object with an animator component that will spawn at Start. This entity will match the game object's position and" +
-             " rotation as well as let that object play animations.")]
-    [SerializeField] private GameObject gameObjectPrefab;
+    [Tooltip(
+        "A Game Object with an animator component that will spawn at Start. This entity will match the game object's position and" +
+        " rotation as well as let that object play animations.")]
+    [SerializeField]
+    private GameObject gameObjectPrefab;
 
     [Tooltip("This will force the game object to match its position and rotation with this entity." +
              "Otherwise, the entity will follow the game object. When an object's position is determined by " +
              "logic on the game object side (like an animator), this should be marked as False.")]
-    [SerializeField] private bool followsEntity; 
+    [SerializeField]
+    private bool followsEntity;
     
-    class Baker : Baker<AnimationAuthoring>
+
+class Baker : Baker<ParticleComponentAuthoring>
     {
-        public override void Bake(AnimationAuthoring authoring)
+        public override void Bake(ParticleComponentAuthoring authoring)
         {
             var entity = GetEntity(TransformUsageFlags.Dynamic);
             AddComponentObject(entity, new GameObjectAnimatorPrefab
@@ -29,57 +34,57 @@ public class AnimationAuthoring : MonoBehaviour
     }
 }
 
-public class GameObjectAnimatorPrefab : IComponentData
+public class GameObjectParticlePrefab : IComponentData
 {
     public GameObject Value;
     public bool FollowEntity;
 }
 
-public class AnimatorReference : ICleanupComponentData
+public class ParticleReference : ICleanupComponentData
 {
-    public Animator Animator;
+    public ParticleSystem Particle;
 }
 
-public partial struct HandleAnimationSystem : ISystem
+public partial struct HandleParticleSystem : ISystem
 {
     public void OnUpdate(ref SystemState state)
     {
         var ecb = new EntityCommandBuffer(state.WorldUpdateAllocator);
 
         // remove game objects when animator reference has been destroyed
-        foreach (var (animatorReference, entity) in
-            SystemAPI.Query<AnimatorReference>()
-                .WithNone<LocalTransform, GameObjectAnimatorPrefab>()
+        foreach (var (particleReference, entity) in
+            SystemAPI.Query<ParticleReference>()
+                .WithNone<LocalTransform, GameObjectParticlePrefab>()
                 .WithEntityAccess())
         {
-            Object.Destroy(animatorReference.Animator.gameObject);
-            ecb.RemoveComponent<AnimatorReference>(entity);
+            Object.Destroy(particleReference.Particle.gameObject);
+            ecb.RemoveComponent<ParticleReference>(entity);
         }
         
         // Add animator references
-        foreach (var (gameObjectPrefab, entity) in SystemAPI.Query<GameObjectAnimatorPrefab>()
-            .WithNone<AnimatorReference>()
+        foreach (var (gameObjectPrefab, entity) in SystemAPI.Query<GameObjectParticlePrefab>()
+            .WithNone<ParticleReference>()
             .WithEntityAccess())
         {
             var gameObjectInstance = Object.Instantiate(gameObjectPrefab.Value);
-            var animatorReference = new AnimatorReference()
+            var particleReference = new ParticleReference()
             {
-                Animator = gameObjectInstance.GetComponent<Animator>()
+                Particle = gameObjectInstance.GetComponent<ParticleSystem>()
             };
-            ecb.AddComponent(entity, animatorReference);
+            ecb.AddComponent(entity, particleReference);
         }
         
         // sync animator transform with corresponding entity transform
-        foreach (var (transform, animatorReference, animatorObject) in
-            SystemAPI.Query<RefRW<LocalTransform>, AnimatorReference, GameObjectAnimatorPrefab>())
+        foreach (var (transform, particleReference, particleObject) in
+            SystemAPI.Query<RefRW<LocalTransform>, ParticleReference, GameObjectParticlePrefab>())
         {
-            var animator = animatorReference.Animator;
+            var animator = particleReference.Particle;
             if (!animator)
                 continue;
             
             var animatorTransform = animator.transform;
             
-            if (animatorObject.FollowEntity)
+            if (particleObject.FollowEntity)
             {
                 animatorTransform.position = transform.ValueRO.Position;
                 animatorTransform.rotation = transform.ValueRO.Rotation;
@@ -90,8 +95,6 @@ public partial struct HandleAnimationSystem : ISystem
                 transform.ValueRW.Rotation = animatorTransform.rotation;
             }
         }
-        
-        
         
         ecb.Playback(state.EntityManager);
         ecb.Dispose();

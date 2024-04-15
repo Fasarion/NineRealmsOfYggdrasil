b7c47b37124198d481bfe253c58bfbf8705c890c
Reciteless <123382507+Reciteless@users.noreply.github.com>
2024-04-15 16:58:56 +0200

    ice2

--- Assets/Scripts/Spawning/SpawningInitializerSystem.cs
+++ Assets/Scripts/Spawning/SpawningInitializerSystem.cs
@@ -80,10 +80,6 @@ public partial class SpawningInitializerSystem : SystemBase
         }
         
         UpdateCheckpointValues(config);
-            
-            
-            
-        
         
     }
 

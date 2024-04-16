04be07723ef113916f6879d12aa112a953caa2f5
pythonpalle <patrikwlindfors@gmail.com>
2024-04-16 11:14:25 +0200

    flyttat om, lagt in animationer

---
+++ Assets/Scripts/Combat/Configs/WeaponStatsSuggestionAuthoring.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class WeaponStatsSuggestionAuthoring : MonoBehaviour
+{
+    [Header("Overall Stats")] 
+    public CombatStats OverallStats;
+    
+    [Header("Attack Stats")]
+    public CombatStats NormalAttack;
+    [Space]
+    public CombatStats SpecialAttack;
+    [Space]
+    public CombatStats PassiveAttack;
+}
+
+[System.Serializable]
+public struct CombatStats
+{
+    [Header("Damage")]
+    public float BaseDamage;
+    public AttackComboMultiplier AttackComboMultiplier; // kanske inte behövs?
+
+    [Header("Critical")]
+    public float CriticalDamage;
+    public float CriticalRate;
+    
+    [Header("Area")]
+    public float Area;
+    
+    [Header("Speed")]
+    public float AttackSpeed;
+    
+    [Header("Cooldown")]
+    public float Cooldown;
+    
+    [Header("Energy")]
+    public float MaxEnergy;
+    public float EnergyFillPerHit;
+}
+
+[System.Serializable]
+public struct AttackComboMultiplier
+{
+    // TODO: Make List?
+    public float AttackOne;
+    public float AttackTwo;
+    public float AttackThree;
+}

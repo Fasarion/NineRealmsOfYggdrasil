751733d198b363f6619f30424f8c58f95f0f0b92
pythonpalle <patrikwlindfors@gmail.com>
2024-04-17 14:42:07 +0200

    disable weapon collision

--- Assets/Scripts/Combat/Attack/PlayerAttackSystem.cs
+++ Assets/Scripts/Combat/Attack/PlayerAttackSystem.cs
@@ -8,16 +8,20 @@ using Unity.Transforms;
 using UnityEngine;
 using Collider = Unity.Physics.Collider;
 using Material = Unity.Physics.Material;
+using Random = UnityEngine.Random;
 
 namespace Patrik
 {
     public partial class PlayerAttackSystem : SystemBase
     {
         private PlayerWeaponManagerBehaviour _weaponManager;
-
         private bool hasSetUpWeaponManager;
 
-        private float disabledTransformScale = 0.001f;
+        CollisionFilter collisionFilter = new CollisionFilter()
+        {
+            BelongsTo = 1, // Projectile
+            CollidesWith = 1 << 1 // Enemy
+        };
 
         protected override void OnUpdate()
         {
@@ -66,17 +70,13 @@ namespace Patrik
 
         private void DisableAllWeapons()
         {
-            var ecb = new EntityCommandBuffer(Allocator.Temp);
-            
-            foreach (var ( transform, entity) in SystemAPI
-                .Query<RefRW<LocalTransform>>()
+            foreach (var ( collider, entity) in SystemAPI
+                .Query<RefRW<PhysicsCollider>>()
                 .WithAll<WeaponComponent>()
                 .WithEntityAccess())
             {
-                transform.ValueRW.Scale = disabledTransformScale;
+                collider.ValueRW.Value.Value.SetCollisionFilter(CollisionFilter.Zero);
             }
-
-            ecb.Playback(EntityManager);
         }
 
         private void SubscribeToAttackEvents()
@@ -106,29 +106,21 @@ namespace Patrik
         private void SetWeaponActive(WeaponType type)
         {
             Entity entity = GetWeaponEntity(type);
-
-            var ecb = new EntityCommandBuffer(Allocator.Temp);
             
             if (SystemAPI.HasComponent<ActiveWeapon>(entity))
             {
                 SystemAPI.SetComponentEnabled<ActiveWeapon>(entity, true);
             }
-            
-            ecb.Playback(EntityManager);
         }
         
         private void SetWeaponPassive(WeaponType type)
         {
             Entity entity = GetWeaponEntity(type);
             
-            var ecb = new EntityCommandBuffer(Allocator.Temp);
-
             if (SystemAPI.HasComponent<ActiveWeapon>(entity))
             {
                 SystemAPI.SetComponentEnabled<ActiveWeapon>(entity, false);
             }
-            
-            ecb.Playback(EntityManager);
         }
 
         private Entity GetWeaponEntity(WeaponType type)
@@ -224,20 +216,17 @@ namespace Patrik
         private void EnableWeapon(WeaponType dataWeaponType)
         {
             Entity entity = GetWeaponEntity(dataWeaponType);
-            SetEntityScale(entity, 1);
-        }
-
-        private void SetEntityScale(Entity entity, float scale)
-        {
-            var transform = EntityManager.GetComponentData<LocalTransform>(entity);
-            transform.Scale = scale;
-            EntityManager.SetComponentData(entity, transform);
+            
+            var collider = SystemAPI.GetComponentRW<PhysicsCollider>(entity);
+            collider.ValueRW.Value.Value.SetCollisionFilter(collisionFilter);
         }
 
         private void DisableWeapon(WeaponType dataWeaponType)
         {
             Entity entity = GetWeaponEntity(dataWeaponType);
-            SetEntityScale(entity, disabledTransformScale);
+            
+            var collider = SystemAPI.GetComponentRW<PhysicsCollider>(entity);
+            collider.ValueRW.Value.Value.SetCollisionFilter(CollisionFilter.Zero);
 
             // clear its hit buffer
             if (EntityManager.HasBuffer<HitBufferElement>(entity))

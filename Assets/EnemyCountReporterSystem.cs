56a158dc39ed32598fb2626d15e4004c31b11675
DataDavid <123382507+Reciteless@users.noreply.github.com>
2024-04-25 11:21:37 +0200

    Update EnemyCountReporterSystem.cs

--- Assets/EnemyCountReporterSystem.cs
+++ Assets/EnemyCountReporterSystem.cs
@@ -6,9 +6,22 @@ using UnityEngine;
 
 public partial class EnemyCountReporterSystem : SystemBase
 {
+    private int _cachedEnemyCount;
     public Action<int> OnEnemyCountChanged;
     protected override void OnUpdate()
     {
-        
+        bool ifSpawnerExists = SystemAPI.TryGetSingleton<SpawnConfig>(out SpawnConfig config);
+        if (!ifSpawnerExists)
+        {
+            // No player level found";
+            return;
+        }
+
+        int currentCount = config.enemyCount;
+        if (_cachedEnemyCount != currentCount)
+        {
+            OnEnemyCountChanged?.Invoke(currentCount);
+            _cachedEnemyCount = currentCount;
+        }
     }
 }

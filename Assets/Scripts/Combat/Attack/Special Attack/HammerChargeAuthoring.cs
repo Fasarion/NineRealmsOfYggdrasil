685af0f66aa0a6a5beb9b47c3192e0439c56b186
pythonpalle <patrikwlindfors@gmail.com>
2024-04-26 22:37:23 +0200

    hammer spec identificator, very buggy atm

---
+++ Assets/Scripts/Combat/Attack/Special Attack/HammerChargeAuthoring.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class HammerChargeAuthoring : MonoBehaviour
+{
+    class Baker : Baker<HammerChargeAuthoring>
+    {
+        public override void Bake(HammerChargeAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent(entity, new HammerChargeComponent());
+        }
+    }
+}
+
+public struct HammerChargeComponent : IComponentData{}
--- Assets/Scripts/Combat/Weapon/Weapon Systems/WeaponAttacks/Hammer/HammerSpecialIndicationSystem.cs
+++ Assets/Scripts/Combat/Weapon/Weapon Systems/WeaponAttacks/Hammer/HammerSpecialIndicationSystem.cs
@@ -1,17 +1,27 @@
-﻿using Patrik;
+﻿using Destruction;
+using Patrik;
 using Player;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Transforms;
 using UnityEngine;
 
+[UpdateAfter(typeof(PlayerRotationSystem))]
 public partial struct HammerSpecialIndicationSystem : ISystem
 {
+    private bool hasInitialized;
+    
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<PlayerTag>();
+        state.RequireForUpdate<PlayerPositionSingleton>();
+        state.RequireForUpdate<PlayerRotationSingleton>();
+        
         state.RequireForUpdate<WeaponAttackCaller>();
         state.RequireForUpdate<HammerComponent>();
+        state.RequireForUpdate<HammerSpecialConfig>();
     }
     
     [BurstCompile]
@@ -20,9 +30,51 @@ public partial struct HammerSpecialIndicationSystem : ISystem
         var attackCaller = SystemAPI.GetSingletonRW<WeaponAttackCaller>();
 
         if (!attackCaller.ValueRO.SpecialChargeInfo.IsChargingWithWeapon(WeaponType.Hammer))
+        {
+            if (hasInitialized)
+            {
+                var ecb = new EntityCommandBuffer(Allocator.Temp);
+                
+                foreach (var (indicator, transform, chargeTimer, entity) in
+                    SystemAPI.Query<RefRW<HammerChargeComponent>, RefRW<LocalTransform>, RefRW<ChargeTimer>>()
+                        .WithEntityAccess())
+                {
+                    ecb.AddComponent<ShouldBeDestroyed>(entity);
+                }
+                
+                ecb.Playback(state.EntityManager);
+                ecb.Dispose();
+
+                hasInitialized = false;
+            }
+
             return;
-        
-        // TODO: spawn indicator, 
-        
+        }
+
+        // initialize indicator
+        if (!hasInitialized)
+        {
+            var config = SystemAPI.GetSingleton<HammerSpecialConfig>();
+            
+            var ability = state.EntityManager.Instantiate(config.IndicatorPrefab);
+            state.EntityManager.SetComponentData(ability, new ChargeTimer
+            {
+                maxChargeTime = 3
+            });
+
+            hasInitialized = true;
+        }
+
+        // make indicator follow player
+        var playerPos = SystemAPI.GetSingleton<PlayerPositionSingleton>();
+        var playerRot = SystemAPI.GetSingleton<PlayerRotationSingleton>();
+        foreach (var (indicator, transform, chargeTimer, entity) in
+            SystemAPI.Query<RefRW<HammerChargeComponent>, RefRW<LocalTransform>, RefRW<ChargeTimer>>()
+                .WithEntityAccess())
+        {
+            transform.ValueRW.Position = playerPos.Value;
+            transform.ValueRW.Rotation = playerRot.Value;
+        }
+
     }
 }

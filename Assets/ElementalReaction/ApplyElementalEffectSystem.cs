b27534d34e9c8848e54913aaa44e243c3c3c455d
DataDavid <123382507+Reciteless@users.noreply.github.com>
2024-04-26 14:29:14 +0200

    asdf

--- Assets/ElementalReaction/ApplyElementalEffectSystem.cs
+++ Assets/ElementalReaction/ApplyElementalEffectSystem.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Damage;
+using Movement;
+using Unity.Burst;
+using Unity.Entities;
 using UnityEngine;
 
-public class ApplyElementalEffectSystem : MonoBehaviour
+[UpdateBefore(typeof(ApplyDamageSystem))]
+[UpdateAfter(typeof(AddElementalEffectSystem))]
+[UpdateInGroup(typeof(CombatSystemGroup))]
+public partial struct ApplyElementalEffectSystem : ISystem
 {
-    // Start is called before the first frame update
-    void Start()
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
     {
-        
+        state.RequireForUpdate<ElementalReactionsConfig>();
     }
 
-    // Update is called once per frame
-    void Update()
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
     {
+        var config = SystemAPI.GetSingleton<ElementalReactionsConfig>();
+        var ecb = new EntityCommandBuffer(state.WorldUpdateAllocator);
         
+        foreach (var (freezeComponent, moveSpeedComponent, affectedEntity)
+                 in SystemAPI.Query<RefRW<ElementalFreezeEffectComponent>, RefRW<MoveSpeedComponent>>().WithEntityAccess())
+        {
+            freezeComponent.ValueRW.CurrentDurationTime += SystemAPI.Time.DeltaTime;
+            
+            if (freezeComponent.ValueRO.CurrentDurationTime > config.FreezeDuration)
+            {
+                
+                ecb.RemoveComponent<ElementalFreezeEffectComponent>(affectedEntity);
+            }
+                
+            if (freezeComponent.ValueRO.HasBeenApplied) continue;
+            
+            
+        }
     }
 }

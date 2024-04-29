994d247b6d6d1c6a7e6d353efdc3c11528e5451f
Fasarion <oscara_93@hotmail.com>
2024-04-29 17:39:54 +0200

    Some updates to weapon switch ui

--- Assets/Individual/Oscar - Programmering/Scripts/CombatUI/CombatUIWeaponHandler.cs
+++ Assets/Individual/Oscar - Programmering/Scripts/CombatUI/CombatUIWeaponHandler.cs
@@ -8,31 +8,37 @@ public class CombatUIWeaponHandler : MonoBehaviour
 {
 
     public WeaponType currentWeapon;
+
+    public WeaponType debugWeaponToUpdateTo;
     //WeaponType currentWeaponType;
     public CombatUIMainWeaponSymbol combatUIMainWeaponSymbol;
 
-    private WeaponType currentLeftInactiveWeapon;
-    private WeaponType currentRightInactiveWeapon;
+    public WeaponType currentLeftInactiveWeapon;
+    public WeaponType currentRightInactiveWeapon;
 
-    public static Action<WeaponType, List<WeaponType>> onCurrentWeaponUpdated;
+    public static Action<WeaponType, WeaponType, WeaponType> onCurrentWeaponUpdated;
     //Placeholder for debugging purposes;
     private WeaponType hammerWeapon = WeaponType.Hammer;
     private WeaponType swordWeapon = WeaponType.Sword;
     private WeaponType birdWeapon = WeaponType.Birds;
-
-   
+    
     //private WeaponType meadWeapon = WeaponType.Mead;
     //I am missing a class to reference to get the current types of weapons from the player
     public List<WeaponType> currentPlayerWeapons;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        currentPlayerWeapons.Add(hammerWeapon);
-        currentPlayerWeapons.Add(swordWeapon);
-        currentPlayerWeapons.Add(birdWeapon);
+        
+        //currentPlayerWeapons.Add(hammerWeapon);
+        //currentPlayerWeapons.Add(swordWeapon);
+        //currentPlayerWeapons.Add(birdWeapon);
     }
 
+    public void DebugButtonPressed()
+    {
+        UpdateCurrentWeapon(debugWeaponToUpdateTo);
+    }
     //We're going to need a list of the players current weapons here.
     public void UpdateCurrentWeapon(WeaponType weaponType)
     {
@@ -40,21 +46,28 @@ public class CombatUIWeaponHandler : MonoBehaviour
         //var selectableWeapons = new List<WeaponType>(currentPlayerWeapons);
         
 
-        //Replace swap inactive and active weapons
+        //Swap inactive and active weapons
         if(currentLeftInactiveWeapon == weaponType)
         {
             currentLeftInactiveWeapon = currentWeapon;
+            Debug.Log("Swapped to left weapon");
         }
         else if (currentRightInactiveWeapon == weaponType)
         {
             currentRightInactiveWeapon = currentWeapon;
+            Debug.Log("Swapped to right weapon");
+        }
+        else
+        {
+            Debug.Log("Something went wrong! You tried to set a weapon that was not one of your inactive weapons!");
         }
         
-        //selectableWeapons.Remove(weaponType);
         currentWeapon = weaponType;
+        
+        
         //currentInactiveWeapons.AddRange(selectableWeapons);
                 
-        //onCurrentWeaponUpdated?.Invoke(weaponType, currentInactiveWeapons);
+        onCurrentWeaponUpdated?.Invoke(currentWeapon, currentLeftInactiveWeapon, currentRightInactiveWeapon);
     }
     // Update is called once per frame
     void Update()

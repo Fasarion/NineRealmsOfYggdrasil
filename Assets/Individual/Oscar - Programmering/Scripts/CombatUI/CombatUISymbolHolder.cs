093d2bf42a9ad26c3d0471193d274eb97ca31e8f
Fasarion <38212985+Fasarion@users.noreply.github.com>
2024-04-30 23:04:24 +0200

    Some progress on symbols for weapon switching

---
+++ Assets/Individual/Oscar - Programmering/Scripts/CombatUI/CombatUISymbolHolder.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatUISymbolHolder : MonoBehaviour
+{
+    
+    public List<Sprite> hammerSymbols;
+    public List<Sprite> swordSymbols;
+    public List<Sprite> meadSymbols;
+    public List<Sprite> birdSymbols;
+}

9ef3c33b2ad2a01879f6d6dd2c4832da93f507a0
Fasarion <oscara_93@hotmail.com>
2024-05-02 20:43:04 +0200

    Updates to the UI

---
+++ Assets/Individual/Oscar - Programmering/Scripts/CombatUI/CombatUIUltWeaponReadyHolder.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Patrik;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombatUIUltWeaponReadyHolder : MonoBehaviour
+{
+    public WeaponType currentWeaponType;
+    public Image imageTarget;
+    public GameObject objectToSet;
+}

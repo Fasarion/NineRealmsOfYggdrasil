56de0372aebcbb203d889da64868258d5d8a8064
pythonpalle <patrikwlindfors@gmail.com>
2024-05-07 17:39:51 +0200

    smooth player walking in different directions

---
+++ Assets/PlayerAnimationMovementSystem.cs
@@ -0,0 +1,56 @@
+﻿using Patrik;
+using Player;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public partial struct PlayerAnimationMovementSystem : ISystem
+{
+    public void OnUpdate(ref SystemState state)
+    {
+        // var tracker = SystemAPI.GetSingletonRW<PlayerMovementTrackerSingletonComponent>();
+
+        if (!SystemAPI.TryGetSingleton(out PlayerMoveInput input)) return;
+        if (!SystemAPI.TryGetSingletonEntity<PlayerTag>(out Entity playerEntity)) return;
+        
+        // TODO: make own 2D based system
+        var playerForward = SystemAPI.GetComponent<LocalTransform>(playerEntity).Forward();
+        
+        Vector3 movement = new Vector3(input.Value.x, 0, input.Value.y);
+        Vector3 lookDirection = playerForward;
+
+        SetMovementInput(movement, lookDirection);
+
+        // float2 rotationAsFloat2 = playerForward.xz;
+
+        //   float rotation = Vector2.SignedAngle(input.Value, rotationAsFloat2);
+        
+        //  var local = input.Value * rotationAsFloat2;
+      
+        //  var local = rotation * input.Value;
+
+        // tracker.ValueRW.WorldSpaceInput = input.Value;
+        // tracker.ValueRW.PlayerForward = rotationAsFloat2;
+        // tracker.ValueRW.LocalSpaceInput = local;
+        //
+        //
+        // PlayerWeaponManagerBehaviour.Instance.SetMovementXY(local); 
+        //PlayerWeaponManagerBehaviour.Instance.SetMovementXY(input.Value); 
+    }   
+    
+    void SetMovementInput(Vector3 movement, Vector3 lookDirection)
+    {
+        float forwardBackwardsMagnitude = 0; 
+        float rightLeftMagnitude = 0;
+        
+        if (movement.magnitude > 0) // magnitude is moar then nothing
+        {
+            forwardBackwardsMagnitude = Mathf.Clamp (Vector3.Dot (movement, lookDirection), -1, 1);
+            Vector3 perpendicularLookingAt = new Vector3 (lookDirection.z, 0, -lookDirection.x);
+            rightLeftMagnitude = Mathf.Clamp (Vector3.Dot (movement, perpendicularLookingAt), -1, 1);
+        }
+
+        PlayerWeaponManagerBehaviour.Instance.SetMovementXY(new float2(rightLeftMagnitude, forwardBackwardsMagnitude));
+    }
+}
--- Assets/Scripts/Player/Player Systems/PlayerMovementSystem.cs
+++ Assets/Scripts/Player/Player Systems/PlayerMovementSystem.cs
@@ -13,13 +13,13 @@ namespace Player
     /// <summary>
     /// System for moving a player based on its move speed and sprint modifier.
     /// </summary>
-    public partial struct PlayerMovementSystem : ISystem 
+    public partial struct PlayerMovementSystem : ISystem
     {
         public void OnCreate(ref SystemState state)
         {
             state.RequireForUpdate<PlayerTag>(); 
             state.RequireForUpdate<PlayerPositionSingleton>(); 
-            state.RequireForUpdate<PlayerMoveInput>(); 
+            state.RequireForUpdate<PlayerMoveInput>();
         }
     
         public void OnUpdate(ref SystemState state)
@@ -33,27 +33,10 @@ namespace Player
                 float speed = speedComp.ValueRO.Value;
 
                 Vector3 moveInputVec3 = new Vector3(moveInput.Value.x, 0, moveInput.Value.y);
-
                 Vector3 step = moveInputVec3 * speed * SystemAPI.Time.DeltaTime;
 
                 animatorReference.Animator.transform.position += step;
-
-                //var newPos = playerTransform.ValueRO.Position.xz +  moveInput.Value * speed * SystemAPI.Time.DeltaTime;
-                //  playerTransform.ValueRW.Position.xz = newPos;
-                //  playerPosSingleton.ValueRW.Value = playerTransform.ValueRO.Position;
                 playerPosSingleton.ValueRW.Value = playerTransform.ValueRO.Position;
-
-
-                
-                if (PlayerWeaponManagerBehaviour.Instance != null)
-                {
-                    bool playerIsMoving = !moveInput.Value.Equals(float2.zero);
-                    
-                    // TODO: lerp
-                    float t = math.length(moveInput.Value);
-                    PlayerWeaponManagerBehaviour.Instance.UpdateMovementParameter(t);
-                }
-                
             }
         }
     }

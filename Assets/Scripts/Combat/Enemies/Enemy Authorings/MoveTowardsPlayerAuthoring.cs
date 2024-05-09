edc934691ebe16aaf610cf213ad5f9df837da38f
pythonpalle <patrikwlindfors@gmail.com>
2024-05-09 11:50:49 +0200

    move away from player speed

--- Assets/Scripts/Combat/Enemies/Enemy Authorings/MoveTowardsPlayerAuthoring.cs
+++ Assets/Scripts/Combat/Enemies/Enemy Authorings/MoveTowardsPlayerAuthoring.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 
 public class MoveTowardsPlayerAuthoring : MonoBehaviour
 {
+    [Tooltip("The minimum distance from the player for this entity to move towards the player.")]
     [SerializeField] private float minimumDistanceForMoving;
+
+    [Tooltip("The speed at which this entity moves away from the player if the player gets to close." +
+             " A value of 0 means the entity will not move away at all.")]
+    [SerializeField] private float awayFromPlayerMoveSpeed = 0.5f;
 
     class Baker : Baker<MoveTowardsPlayerAuthoring>
     {
@@ -14,7 +19,8 @@ public class MoveTowardsPlayerAuthoring : MonoBehaviour
             var entity = GetEntity(TransformUsageFlags.Dynamic);
             AddComponent(entity, new MoveTowardsPlayerComponent
             {
-                MinimumDistanceForMovingSquared = authoring.minimumDistanceForMoving * authoring.minimumDistanceForMoving
+                MinimumDistanceForMovingSquared = authoring.minimumDistanceForMoving * authoring.minimumDistanceForMoving,
+                MoveAwayFromPlayerSpeed = authoring.awayFromPlayerMoveSpeed
             });
         }
     }
--- Assets/Scripts/Combat/Enemies/Enemy Systems/MoveTowardsPlayerSystem.cs
+++ Assets/Scripts/Combat/Enemies/Enemy Systems/MoveTowardsPlayerSystem.cs
@@ -22,28 +22,25 @@ public partial struct MoveTowardsPlayerSystem : ISystem
     {
         float3 playerPos = SystemAPI.GetSingleton<PlayerPositionSingleton>().Value;
         float deltaTime = SystemAPI.Time.DeltaTime;
-
-        float directionMultiplier = 1;
-
+        
         foreach (var (transform, moveSpeed, moveToPlayer) 
             in SystemAPI.Query<RefRW<LocalTransform>, MoveSpeedComponent, MoveTowardsPlayerComponent>().WithNone<HitStopComponent>())
         {
+            float speed = moveSpeed.Value;
+            
             var distanceToPlayer = math.distancesq(playerPos, transform.ValueRO.Position);
             if (distanceToPlayer < moveToPlayer.MinimumDistanceForMovingSquared)
             {
-                directionMultiplier = -1;
+                speed = -moveToPlayer.MoveAwayFromPlayerSpeed;
             }
-            else
-            {
-                directionMultiplier = 1;
-            }
+            
             
             var direction = playerPos - transform.ValueRO.Position;
             direction.y = 0;
             quaternion lookRotation = math.normalizesafe(quaternion.LookRotation(direction, math.up()));
             
             transform.ValueRW.Rotation = lookRotation;
-            transform.ValueRW.Position += math.normalize(direction) * moveSpeed.Value * deltaTime * directionMultiplier;
+            transform.ValueRW.Position += math.normalize(direction) * speed * deltaTime;
         }
     }
 }

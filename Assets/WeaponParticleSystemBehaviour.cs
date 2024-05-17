a0ae75c9acff21405093082a091cacf759d98dae
pythonpalle <patrikwlindfors@gmail.com>
2024-05-17 20:04:19 +0200

    vfx weapon trail on sword attacks

---
+++ Assets/WeaponParticleSystemBehaviour.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Patrik;
+using UnityEngine;
+
+public class WeaponParticleSystemBehaviour : MonoBehaviour
+{
+    [SerializeField] private WeaponType weaponType;
+    [SerializeField] private ParticleSystem _particleSystem;
+    
+    [Tooltip("Which attack types will trigger this particle system to activate?")]
+    [SerializeField] private List<AttackType> attacksToActivateParticles;
+
+    private void OnEnable()
+    {
+        EventManager.OnActiveAttackStart += OnActiveAttackStart;
+        EventManager.OnActiveAttackStop += OnActiveAttackStop;
+        
+        EventManager.OnPassiveAttackStart += OnPassiveAttackStart;
+        EventManager.OnPassiveAttackStop += OnPassiveAttackStop;
+    }
+    
+    private void OnDisable()
+    {
+        EventManager.OnActiveAttackStart -= OnActiveAttackStart;
+        EventManager.OnActiveAttackStop -= OnActiveAttackStop;
+
+        EventManager.OnPassiveAttackStart -= OnPassiveAttackStart;
+        EventManager.OnPassiveAttackStop -= OnPassiveAttackStop;
+    }
+
+    void HandleParticleActivision(AttackData attackData, bool activate)
+    {
+        if (attackData.WeaponType != weaponType) return;
+
+        if (attacksToActivateParticles.Contains(attackData.AttackType))
+        {
+            _particleSystem.gameObject.SetActive(activate);
+        }
+    }
+
+    private void OnActiveAttackStart(AttackData attackData)
+    {
+        HandleParticleActivision(attackData, true);
+    }
+    
+    private void OnActiveAttackStop(AttackData attackData)
+    {
+        HandleParticleActivision(attackData, false);
+    }
+    
+    private void OnPassiveAttackStart(AttackData attackData)
+    {
+        HandleParticleActivision(attackData, true);
+    }
+    
+    private void OnPassiveAttackStop(AttackData attackData)
+    {
+        HandleParticleActivision(attackData, false);
+    }
+}

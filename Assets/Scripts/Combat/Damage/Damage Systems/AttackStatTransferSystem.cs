e821047d2b2ec230b3a1febb8edb08d5888612da
pythonpalle <patrikwlindfors@gmail.com>
2024-05-23 09:45:18 +0200

    genereal tranferer system

--- Assets/Scripts/Combat/Damage/Damage Systems/AttackStatTransferSystem.cs
+++ Assets/Scripts/Combat/Damage/Damage Systems/AttackStatTransferSystem.cs
@@ -12,79 +12,42 @@ public partial struct AttackStatTransferSystem : ISystem
     {
         var ecb = new EntityCommandBuffer(state.WorldUpdateAllocator);
 
-        // transfer damage
+        // transfer component data
         foreach (var  (updateStats, receiverEntity) in SystemAPI
             .Query<UpdateStatsComponent>().WithEntityAccess())
         {
             Entity transfererEnttiy = updateStats.EntityToTransferStatsFrom;
 
-            // Transfer damage
-            if (state.EntityManager.HasComponent<CachedDamageComponent>(transfererEnttiy))
-            {
-                var transferComponent = state.EntityManager.GetComponentData<CachedDamageComponent>(transfererEnttiy);
-                
-                if (!state.EntityManager.HasComponent<CachedDamageComponent>(receiverEntity))
-                    ecb.AddComponent<CachedDamageComponent>(receiverEntity);
-                
-                ecb.SetComponent(receiverEntity, transferComponent);
-            }
-            
-            // Transfer knock back
-            if (state.EntityManager.HasComponent<KnockBackOnHitComponent>(transfererEnttiy))
-            {
-                var transferComponent = state.EntityManager.GetComponentData<KnockBackOnHitComponent>(transfererEnttiy);
-                
-                if (!state.EntityManager.HasComponent<KnockBackOnHitComponent>(receiverEntity))
-                    ecb.AddComponent<KnockBackOnHitComponent>(receiverEntity);
-                
-                ecb.SetComponent(receiverEntity, transferComponent);
-            }
-            
-            // Transfer hit stun
-            if (state.EntityManager.HasComponent<ShouldApplyHitStopOnHit>(transfererEnttiy))
-            {
-                var transferComponent = state.EntityManager.GetComponentData<ShouldApplyHitStopOnHit>(transfererEnttiy);
-                
-                if (!state.EntityManager.HasComponent<ShouldApplyHitStopOnHit>(receiverEntity))
-                    ecb.AddComponent<ShouldApplyHitStopOnHit>(receiverEntity);
-                
-                ecb.SetComponent(receiverEntity, transferComponent);
-            }
+            TryTransferComponentData<CachedDamageComponent>(state, transfererEnttiy, receiverEntity, ecb);
+            TryTransferComponentData<KnockBackOnHitComponent>(state, transfererEnttiy, receiverEntity, ecb);
+            TryTransferComponentData<ShouldApplyHitStopOnHit>(state, transfererEnttiy, receiverEntity, ecb);
+            TryTransferComponentData<ElementalShouldApplyFireComponent>(state, transfererEnttiy, receiverEntity, ecb);
+            TryTransferComponentData<ElementalShouldApplyIceComponent>(state, transfererEnttiy, receiverEntity, ecb);
         }
-        
-        // // transfer damage
-        // foreach (var (damageTransferReceiver, updateStats) in SystemAPI
-        //     .Query<RefRW<CachedDamageComponent>, UpdateStatsComponent>())
-        // {
-        //     var damageToTransfer = state.EntityManager.GetComponentData<CachedDamageComponent>(updateStats.EntityToTransferStatsFrom);
-        //     damageTransferReceiver.ValueRW.Value = damageToTransfer.Value;
-        // }
-        //
-        // // transfer hit stun
-        // foreach (var (damageTransferReceiver, updateStats) in SystemAPI
-        //     .Query<RefRW<ShouldApplyHitStopOnHit>, UpdateStatsComponent>())
-        // {
-        //     Entity receiverEntity = updateStats.EntityToTransferStatsFrom;
-        //
-        //     if (!state.EntityManager.HasComponent<ShouldApplyHitStopOnHit>(receiverEntity))
-        //     {
-        //         ecb.AddComponent<ShouldApplyHitStopOnHit>(receiverEntity);
-        //     }
-        //     
-        //     var damageToTransfer = state.EntityManager.GetComponentData<CachedDamageComponent>(updateStats.EntityToTransferStatsFrom);
-        //     damageTransferReceiver.ValueRW.Value = damageToTransfer.Value;
-        // }
 
-        
         // remove update stats tags
         foreach (var (_, entity) in SystemAPI.Query<UpdateStatsComponent>()
-                .WithAll<UpdateStatsComponent>()
-                .WithEntityAccess())
+                .WithAll<UpdateStatsComponent>().WithEntityAccess())
         {
             ecb.RemoveComponent<UpdateStatsComponent>(entity);
         }
         
         ecb.Playback(state.EntityManager);
         ecb.Dispose();
+    }
+
+    private void TryTransferComponentData<T>(SystemState state, Entity transfererEnttiy, Entity receiverEntity,
+        EntityCommandBuffer ecb) where T : unmanaged, IComponentData
+    {
+        if (state.EntityManager.HasComponent<T>(transfererEnttiy))
+        {
+            var transferComponent = state.EntityManager.GetComponentData<T>(transfererEnttiy);
+
+            // Add component to receiver if it doesn't already have it
+            if (!state.EntityManager.HasComponent<T>(receiverEntity))
+                ecb.AddComponent<T>(receiverEntity);
+
+            ecb.SetComponent(receiverEntity, transferComponent);
+        }
     }
 }

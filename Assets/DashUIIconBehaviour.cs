30060758cbea91c777229d962a1a970f987ae65b
pythonpalle <patrikwlindfors@gmail.com>
2024-05-29 21:17:25 +0200

    Dash UI

--- Assets/DashUIIconBehaviour.cs
+++ Assets/DashUIIconBehaviour.cs
@@ -1,16 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class DashUIIconBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Fill")]
+    [SerializeField] private Image fillImage;
+    
+    [Header("Dash")]
+    [SerializeField] private Image dashIcon;
+    
+    [Header("Color")]
+    [SerializeField] private float alphaWhenNotReady = 0.5f;
+    
+    public void UpdateInfo(DashInfoElement dashInfoElement)
     {
+        var dashInfo = dashInfoElement.Value;
         
+        bool ready = dashInfo.Ready;
+
+        fillImage.fillAmount = dashInfo.CurrentTime / dashInfo.CooldownTime;
+
+        float alpha = ready ? 1 : alphaWhenNotReady;
+
+        UpdateImageColor(dashIcon, alpha);
+        UpdateImageColor(fillImage, alpha);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateImageColor(Image image, float alpha)
     {
-        
+        var oldColor = image.color;
+        image.color = new Color(oldColor.r, oldColor.g, oldColor.b, alpha);
     }
 }

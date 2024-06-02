5fc1477f00ad7cdb0ac5094f0547ec6da05dec4c
pythonpalle <patrikwlindfors@gmail.com>
2024-06-02 10:31:00 +0200

    change player speed from animation

--- Assets/Scripts/Combat/Energy/Energy MonoBehaviours/EventManager.cs
+++ Assets/Scripts/Combat/Energy/Energy MonoBehaviours/EventManager.cs
@@ -46,6 +46,9 @@ public static class EventManager
     public static UnityAction OnScreenFadeComplete;
 
     public static UnityAction OnPlayerDeath;
+    
+    public static UnityAction<MoveSpeedChangeData> OnChangeMoveSpeed;
+    public static UnityAction OnResetMoveSpeed;
 }
 
 public struct PlayerHealthData

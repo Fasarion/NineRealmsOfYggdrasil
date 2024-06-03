16f64ee738b6572b6e5f66505b7b8d9a75f1b56f
Fasarion <38212985+Fasarion@users.noreply.github.com>
2024-06-04 01:00:37 +0200

    Updates to prepare for VisualEffects on middle symbols

--- Assets/Individual/Oscar - Programmering/Scripts/CombatUI/CombatUIUltWeaponReadyHolder.cs
+++ Assets/Individual/Oscar - Programmering/Scripts/CombatUI/CombatUIUltWeaponReadyHolder.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using Patrik;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.VFX;
 
 public class CombatUIUltWeaponReadyHolder : MonoBehaviour
 {
     public WeaponType currentWeaponType;
-    public Image imageTarget;
-    public GameObject objectToSet;
+    //public Image imageTarget;
+    //public GameObject objectToSet;
+    public VisualEffect vfx;
+
+    public bool shouldPlay;
+    
+    public void PlaySystem()
+    {
+       
+        vfx.SendEvent("OnPlay");
+        
+        
+    }
+    
+    public void StopSystem()
+    {
+        vfx.SendEvent("OnStop");
+    }
 }

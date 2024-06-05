f163d528086d40750aeaed815e888e05a08a7458
pythonpalle <patrikwlindfors@gmail.com>
2024-06-05 21:10:29 +0200

    hammer passive sound

--- Assets/Scripts/Combat/Weapon/Weapon Systems/WeaponAttacks/Hammer/HammerPassiveAttackSystem.cs
+++ Assets/Scripts/Combat/Weapon/Weapon Systems/WeaponAttacks/Hammer/HammerPassiveAttackSystem.cs
@@ -31,6 +31,8 @@ public partial struct HammerPassiveAttackSystem : ISystem
         {
             return;
         }
+        
+        Debug.Log("Hammer passive");
 
         var ecb = new EntityCommandBuffer(state.WorldUpdateAllocator);
         var config = SystemAPI.GetSingleton<HammerPassiveAbilityConfig>();

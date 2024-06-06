1534c6d4e7c41bb718fa34a3bd1eec16ee6ff05a
Fasarion <38212985+Fasarion@users.noreply.github.com>
2024-06-06 17:13:36 +0200

    Added tutorial texts for new weapon and special

---
+++ Assets/Individual/Oscar - Programmering/Scripts/CombatUI/Tutorial/UnlockedNewWeaponSubSegment.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Patrik;
+using UnityEngine;
+
+public class UnlockedNewWeaponSubSegment : MonoBehaviour
+{
+    public NewAbilityUnlockTutorialSegment abilityUnlockTutorialSegment;
+    public List<GameObject> weaponUnlockObjects;
+
+    private bool weaponUnlocked;
+    public void Start()
+    {
+        abilityUnlockTutorialSegment = FindObjectOfType<NewAbilityUnlockTutorialSegment>();
+        weaponUnlocked = false;
+        for (int i = 0; i < weaponUnlockObjects.Count; i++)
+        {
+            weaponUnlockObjects[i].SetActive(false);
+        }
+    }
+
+    public void OnEnable()
+    {
+        EventManager.OnWeaponCountSet += OnWeaponUnlocked;
+        EventManager.OnWeaponSwitch += OnWeaponSwitchActivated;
+    }
+
+    private void OnWeaponSwitchActivated(WeaponSetupData currentWeapon, List<WeaponSetupData> weaponSetupDataList)
+    {
+        if (weaponUnlocked)
+        {
+            for (int i = 0; i < weaponUnlockObjects.Count; i++)
+            {
+                weaponUnlockObjects[i].SetActive(false);
+            }
+          
+            abilityUnlockTutorialSegment.SecondWeaponUnlocked(true);
+        }
+        
+    }
+
+    private void OnWeaponUnlocked(int weaponCount)
+    {
+        if (weaponUnlocked != true)
+        {
+            weaponUnlocked = true;
+            for (int i = 0; i < weaponUnlockObjects.Count; i++)
+            {
+                weaponUnlockObjects[i].SetActive(true);
+            }
+        }
+       
+        
+    }
+}

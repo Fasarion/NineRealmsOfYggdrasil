9ff816f19fbeb25bdc3799f015d3ec0ce523f582
Fasarion <38212985+Fasarion@users.noreply.github.com>
2024-07-21 17:55:59 +0200

    Updated so that victory screen takes a couple secs before showing up

--- Assets/Individual/Oscar - Programmering/Scripts/Menus/ObjectiveCompleted.cs
+++ Assets/Individual/Oscar - Programmering/Scripts/Menus/ObjectiveCompleted.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class ObjectiveCompleted : MonoBehaviour
 {
     public VictoryUI victoryUI;
-    
+    public float timeToWaitBeforeVictory;
     public void Start()
     {
         victoryUI = FindObjectOfType<VictoryUI>(true);
@@ -25,7 +25,24 @@ public class ObjectiveCompleted : MonoBehaviour
 
     private void OnObjectiveReached()
     {
+        StartCoroutine(WaitForVictory());
+       
+    }
+
+    private IEnumerator WaitForVictory()
+    {
+        bool needToWait = true;
+        while (needToWait)
+        {
+            yield return new WaitForSeconds(timeToWaitBeforeVictory);
+
+            needToWait = false;
+        }
         victoryUI.gameObject.SetActive(true);
         Time.timeScale = 0;
+        
+       
+        
+        
     }
 }
